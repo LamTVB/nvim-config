998deb83c75bf41c4d11faf0c7f4ece5e676245e
Lam <lam@unito.io>
2025-04-04 18:02:41 -0400

    Add keymaps based on muscle memory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del("n", "<leader>fF")
+vim.keymap.del("v", "s")
+vim.keymap.del("n", "s")
+vim.keymap.set("n", " ", '<PageDown>', { noremap = true, silent = true })
+vim.keymap.set("n", "-", '<PageUp>', { noremap = true, silent = true })
