4cc565ff33ea90037922c7bbc5cf833ff3c3ec6e
Lam <lam@unito.io>
2025-12-24 05:06:18 -0500

    Do not auto attach debugger all the time

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,12 @@ vim.keymap.del("v", "s")
 vim.keymap.del("n", "s")
 vim.keymap.set({ "n", "v" }, " ", "<PageDown>", { noremap = true, silent = true })
 vim.keymap.set("n", "-", "<PageUp>", { noremap = true, silent = true })
+
+-- Copy pase without overwriting the default register
 vim.keymap.set("x", "p", function()
   return "pgv\"" .. vim.v.register .. "y`>"
 end, { expr = true, noremap = true, silent = true })
+
 vim.keymap.set("v", "p", function()
   return "pgv\"" .. vim.v.register .. "y`>"
 end, { expr = true, noremap = true, silent = true })
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -18,19 +18,6 @@ return {
 
     dapui.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-
     dap.adapters["pwa-node"] = {
       type = 'server',
       host = "localhost",
