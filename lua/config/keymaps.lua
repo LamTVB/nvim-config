4c9497713aedf99604035472b062ea14d754792a
Lam <lam@unito.io>
2026-01-14 17:24:37 -0500

    Update paste behaviour

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,11 +7,5 @@ vim.keymap.del("n", "s")
 vim.keymap.set({ "n", "v" }, " ", "<PageDown>", { noremap = true, silent = true })
 vim.keymap.set("n", "-", "<PageUp>", { noremap = true, silent = true })
 
--- Copy pase without overwriting the default register
-vim.keymap.set("x", "p", function()
-  return "pgv\"" .. vim.v.register .. "y`>"
-end, { expr = true, noremap = true, silent = true })
-
-vim.keymap.set("v", "p", function()
-  return "pgv\"" .. vim.v.register .. "y`>"
-end, { expr = true, noremap = true, silent = true })
+-- Paste without overwriting the default register
+vim.keymap.set("x", "p", [["_dP]])
